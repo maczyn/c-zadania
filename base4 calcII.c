997f7297e51aacb9227fe7419bae1123f0178772
maciek <maciekzyngiel@gmail.com>
2018-01-09 23:37:16 +0100

    base4 calcII

---
+++ base4 calcII.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+char base4_1[]="00";
+char base4_2[]="00";
+int a, b, x, y, i=0;
+
+int conv1(char tab[2])
+{
+
+        switch(tab[i])
+        {
+            case '0':
+                x=0;
+                break;
+            case '1':
+                x=1;
+                break;
+            case '2':
+                x=2;
+                break;
+            case '3':
+                x=3;
+                break;
+
+
+        }
+        i++;
+        switch(tab[i])
+        {
+            case '0':
+                y=0;
+                break;
+            case '1':
+                y=1;
+                break;
+            case '2':
+                y=2;
+                break;
+            case '3':
+                y=3;
+                break;
+        }
+    return x*4+y;
+
+}
+
+int main()
+{
+    printf("podaj liczbe w base4\n");
+    scanf ("%c%c", &base4_1[0], &base4_1[1]);
+
+    a=conv1(base4_1);
+    printf("%d\n",base4_1);
+
+
+    printf("podaj kolejna liczbe w base4\n");
+    scanf ("%c%c", &base4_2[0], &base4_2[1]);
+
+    b=conv1(base4_2);
+
+    printf("%d\n",base4_2);
+
+    return 0;
+}
